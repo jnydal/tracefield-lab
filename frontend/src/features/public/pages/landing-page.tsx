30f52bc7da1c94fe9e84efda245fe4efe1758684
Thor Nydal <thor.nydal@sikt.no>
2026-02-12 11:25:43 +0100

    added bundling of frontend

--- frontend/src/features/public/pages/landing-page.tsx
+++ frontend/src/features/public/pages/landing-page.tsx
@@ -1,5 +1,5 @@
 import { Button } from 'flowbite-react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { PublicFooter } from '../../../components/public-footer';
 
 export function LandingPage() {
